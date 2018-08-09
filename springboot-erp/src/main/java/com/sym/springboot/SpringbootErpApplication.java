d47c29d0081660b98f590f06123d9dd2ced1d647
宋益民 <song.yimin@cesgroup.com.cn>
2018-08-09 19:37:30 +0800

    .

--- springboot-erp/src/main/java/com/sym/springboot/SpringbootErpApplication.java
+++ springboot-erp/src/main/java/com/sym/springboot/SpringbootErpApplication.java
@@ -3,9 +3,11 @@ package com.sym.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.sym")
 public class SpringbootErpApplication {
 
 	public static void main(String[] args) {
